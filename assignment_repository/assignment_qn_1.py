4080319bae3d6c0ee44121b71a72506dff6e2c41
Kimuda <pjkanywa@gmail.com>
2015-03-20 19:47:45 +0200

    assignment

---
+++ assignment_repository/assignment_qn_1.py
@@ -0,0 +1,6 @@
+counter=99
+
+while counter>=1:
+    print(counter,"bottles of beer on the wall,",counter,"bottles of beer on the wall.","Take one down, pass it around,",counter-1,"bottles of beer on the wall.")
+    print() #not really necessary, but it makes for easier reading.
+    counter=counter-1
