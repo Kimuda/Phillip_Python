af0c0cb88b882e203c9b3f3cd29875440cec5b03
Kimuda <pjkanywa@gmail.com>
2015-03-21 20:32:47 +0200

    assignment

--- assignment_repository/assignment_qn3.py
+++ assignment_repository/assignment_qn3.py
@@ -1,5 +1,5 @@
 text=input("please enter palindrome text ")
-textlower=text.lower() #this is to avoid Radar not being equal to raDar
+textlower=text.lower() #this is to avoid Radar not being equal to raDaR
 reversetextlower=""
 
 for i in range(1,len(textlower)+1):
