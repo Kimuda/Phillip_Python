6f491b09a8b9159826c0190de497982c5110618f
Kimuda <pjkanywa@gmail.com>
2015-03-25 15:00:01 +0200

    m

---
+++ Basic_concepts/6_the_gray.py
@@ -0,0 +1,1 @@
+print("Once more into the fray\nInto the last good fight I'll ever know\nLive or die on this day\nLive or die on this day\n")
---
+++ Basic_concepts/6_trial_gray.py
@@ -0,0 +1,5 @@
+print("Once more into the fray")
+print("Into the last good fight I'll ever know")
+print("Live or die on this day")
+print("Live or die on this day")
+
