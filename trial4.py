966b5fbc74dbe26e8d514ddffbe3a1f3fd06ec5e
Kimuda <pjkanywa@gmail.com>
2015-03-25 14:50:55 +0200

    reload

---
+++ trial4.py
@@ -0,0 +1,14 @@
+
+#Write a program that accepts a list of numbers terminated by a blank line. Print out the entered numbers as elements of a tuple, in the order they were entered.
+
+number=int(input("enter a number"))
+
+while number!="":
+    
+    for in range(number):
+        t=(i,)
+    print(t)
+
+print()   
+
+    #number=int(input("enter a number"))
