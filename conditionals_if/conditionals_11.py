041a5b13cc576e76f66721daad3951fd5d2b5d2a
Kimuda <pjkanywa@gmail.com>
2015-03-25 19:37:10 +0200

    with test qns

---
+++ conditionals_if/conditionals_11.py
@@ -0,0 +1,8 @@
+value=int(input('enter a number '))
+list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in list:
+    if value%i==0:
+        print(i)
+    else:
+        print()
+    
---
+++ conditionals_if/conditionals_6.py
@@ -0,0 +1,18 @@
+v=int(input("enter first number "))
+w=int(input("enter second number "))
+x=int(input("enter third number "))
+y=int(input("enter fourth number "))
+z=int(input("enter fifth number "))
+
+if v>w and v>x and v>y and v>z:
+    print(v)
+elif w>v and w>x and w>y and w>z:
+    print(w)
+elif x>v and x>w and x>y and x>z:
+    print(x)
+elif y>v and y>w and y>x and y>z:
+    print(y)
+elif z>v and z>w and z>x and z>y:
+    print(z)
+
+
