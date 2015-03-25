a2487926a583a2bd66dd46ce00dc97adba5f2723
Kimuda <pjkanywa@gmail.com>
2015-03-25 19:34:40 +0200

    with test qns

---
+++ basic_input/7.py
@@ -0,0 +1,11 @@
+
+v=float(input("enter first number "))
+w=float(input("enter second number "))
+x=float(input("enter third number "))
+y=float(input("enter fourth number "))
+z=float(input("enter fifth number "))
+
+total=v+w+x+y+z
+print("Total=",total)
+average=total/5
+print("Average=",average)
